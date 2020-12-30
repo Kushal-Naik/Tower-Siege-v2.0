66a1e353d5ae1fffe2f8b0b61a06494208af6e95
Kushal Naik <70422748+Kushal-Naik@users.noreply.github.com>
2020-12-30 20:44:42 +0530

    Add files via upload

---
+++ boxpink.js
@@ -0,0 +1,30 @@
+class Box1 extends BaseClass 
+{
+  constructor(x,y,width,height)
+  {
+    super(x,y,width,height);
+    this.image = loadImage("pinkBlock.png");
+    this.visibility = 255;
+  }
+
+  display()
+  {
+    if(this.body.speed > 3){
+      World.remove(world, this.body);
+      push();
+      this.visibility = this.visibility -5;
+      tint(255, this.visibility);
+      image(this.image, this.body.position.x, this.body.position.y, this.width, this.height);
+      pop();
+    }
+    else{
+      imageMode(CENTER);
+      image(this.image, this.body.position.x, this.body.position.y, this.width, this.height);
+      super.display();
+    }
+
+    // fill(230, 190, 234)
+    // super.display();
+  }
+}
+     
